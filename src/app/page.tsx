1dd535de05888448fc4b74a3dbfcc098a2274882
Serge Basangovs <sergejs.basangovs@gmail.com>
2025-07-18 20:46:58 +0300

    chore: reverted to basic setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,7 @@ export const metadata: Metadata = {}
 const HomePage = async () => {
   return (
     <>
-      <h1 className="text-3xl font-bold text-center my-8 underline">
-        Hi Games!
-      </h1>
+      <h1 className="text-3xl font-bold text-center my-8 underline">Hi!</h1>
     </>
   )
 }
