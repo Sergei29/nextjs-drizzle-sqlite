b1346d47dcc9ea5d04ccd4dbf39d39cb1651e992
Serge Basangovs <sergejs.basangovs@gmail.com>
2025-07-19 12:54:55 +0300

    chore: installed ui components, formatted

--- src/components/providers/ThemeProvider/index.tsx
+++ src/components/providers/ThemeProvider/index.tsx
@@ -1,14 +1,20 @@
 "use client"
 
 import React, { type ComponentProps } from "react"
-
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+import { Toaster } from "@/components/ui/sonner"
 
 const ThemeProvider = ({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) => {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props}>
+      {children}
+      <Toaster />
+    </NextThemesProvider>
+  )
 }
 
 export default ThemeProvider
